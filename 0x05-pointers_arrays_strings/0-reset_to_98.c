3a49070dc4e77d5cbc879cdb0ef073e8638f47e6
Lucas Chacha <lykmoronge@gmail.com>
2023-08-21 01:58:31 -0700

    a function that takes a pointer to an int as parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a fun that returns a pointer to an int as parameter
+ * and changes the the parameter it points to to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#C - Pointers, arrays and strings
+11 project tasks on the a bove topic
