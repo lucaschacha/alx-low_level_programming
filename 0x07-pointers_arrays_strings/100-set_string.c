51f87c137b20a4ddee2f39fe7e95fbe19debdfcf
Lucas Chacha <lykmoronge@gmail.com>
2023-08-30 13:39:17 -0700

    print_diagsums

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -8,19 +8,19 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, c;
+	int sum1, sum2, y;
 
 	sum1 = 0;
 	sum2 = 0;
 
-	for (c = 0; c < size; c++)
+	for (y = 0; y < size; y++)
 	{
-		sum1 = sum1 + a[c * size + c];
+		sum1 = sum1 + a[y * size + y];
 	}
 
-	for (c = size - 1; c >= 0; c--)
+	for (y = size - 1; y >= 0; y--)
 	{
-		sum2 += a[c * size + (size - c - 1)];
+		sum2 += a[y * size + (size - y - 1)];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
