072c20f63365c369f5cd26cae07e067ff957fbd7
Lucas Chacha <lykmoronge@gmail.com>
2023-08-30 13:22:40 -0700

    a function that sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Starting point
+ * @a: input value
+ * @size: input value
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, c;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (c = 0; c < size; c++)
+	{
+		sum1 = sum1 + a[c * size + c];
+	}
+
+	for (c = size - 1; c >= 0; c--)
+	{
+		sum2 += a[c * size + (size - c - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
